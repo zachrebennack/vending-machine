1a4571e80a8b07855c66827d1d5cfe6c34d35a9c
zachrebennack <zachary.rebennack@infinite-acres.com>
2024-09-13 22:54:06 -0400

    added deposit function

--- main_menu.py
+++ main_menu.py
@@ -11,6 +11,7 @@ def main_menu():
             inventory_selection()
         elif menu_choice == 2:
             print("You chose Deposit Money")
+            deposit()
         elif menu_choice == 3:
             print("You chose Return Change")
         elif menu_choice == 4:
@@ -26,6 +27,39 @@ def inventory_selection():
 
     # Print the DataFrame as a table
     print(df)
+
+def deposit():
+    balance = 0
+    while True:
+        try: 
+            bills = int(input("How much money in bills are you depositing? "))
+            break #exit the loop if whole number entered
+        except ValueError:
+            print("Please enter a whole number!")
+    balance += bills 
+    while True:
+        try: 
+            quarters = float(input("How many quarters are you depositing? "))
+            break #exit the loop if whole number entered
+        except ValueError:
+            print("Please enter a whole number!")
+    balance += quarters * 0.25
+    while True:
+        try: 
+            dimes = float(input("How many dimes are you depositing? "))
+            break #exit the loop if whole number entered
+        except ValueError:
+            print("Please enter a whole number!")
+    balance += dimes * 0.1
+    while True:
+        try: 
+            nickels = float(input("How many nickels are you depositing? "))
+            break #exit the loop if whole number entered
+        except ValueError:
+            print("Please enter a whole number!")
+    balance += nickels * 0.05
+    print("\nYou have entered $" + str(f"{balance:.2f}") + "\n")
+    main_menu()
 
 if __name__ == "__main__":
     main_menu()
